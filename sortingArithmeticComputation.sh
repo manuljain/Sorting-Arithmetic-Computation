08a4aa2f1bf62acca5bde7816e8134c49fddaa01
Manul <manuljain16@gmail.com>
2020-10-20 19:33:38 +0530

    Added three user input numbers

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -2,3 +2,10 @@
 
 # Welcome message
 echo "Welcome to Sorting Arithmetic Computation Program."
+
+# UC-1 Input three numbers
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c
+
+echo "Numbers are : $a, $b, $c"
