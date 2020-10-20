edca67d6a54ac5ce690a8f03d47f4ffc3b10fdf1
Manul <manuljain16@gmail.com>
2020-10-20 20:20:07 +0530

    Added array for storing results

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -10,6 +10,7 @@ read -p "Enter third number : " c
 
 # display values
 echo "Numbers are : $a, $b, $c"
+echo "========================================="
 
 #computing
 n1=$(($a+$b*$c))
@@ -22,6 +23,7 @@ echo "$a + $b * $c = $n1"
 echo "$a * $b + $c = $n2"
 echo "$c + $a / $b = $n3"
 echo "$a % $b + $c = $n4"
+echo "========================================="
 
 # storing results in dictionary
 dict[1]="$n1"
@@ -29,7 +31,16 @@ dict[2]="$n2"
 dict[3]="$n3"
 dict[4]="$n4"
 
-
 #display dictionay
 echo "keys of dictionary : "${!dict[@]}
 echo "values of dictionary : "${dict[@]}
+echo "========================================="
+
+# storing values of dictionary in array
+for (( i=1; i<=${#dict[@]}; i++ ))
+do
+    arr[$i]="${dict[$i]}"
+done
+
+# display array
+echo "elements of array : "${arr[@]}
