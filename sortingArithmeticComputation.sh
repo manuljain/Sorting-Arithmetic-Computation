63ccba3a8999dd975a65b0cb02d9073b30b64f14
Manul <manuljain16@gmail.com>
2020-10-20 19:47:42 +0530

    Added first arithmetic operation

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -8,4 +8,11 @@ read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c
 
+# display values
 echo "Numbers are : $a, $b, $c"
+
+#computing
+n1=$(($a+$b*$c))
+
+#computed value
+echo "$a + $b * $c = $n1"
